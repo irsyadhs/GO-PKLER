86956b39bf0b2f6d99bd410b63f43231f6e55432
faisalsyfl <faisalsyfl@gmail.com>
2017-04-22 18:51:46 +0700

    Initial commit

---
+++ app/src/main/java/go_pkler/irsyaddhs/cs/upi/edu/go_pkler/AppConfig.java
@@ -0,0 +1,15 @@
+package go_pkler.irsyaddhs.cs.upi.edu.go_pkler;
+
+/**
+ * Created by Faisal Syaiful Anwar on 4/22/2017.
+ */
+
+public class AppConfig {
+
+    public static final String TAG = "go-pkler";
+    public static final String SP = "go_pkler.irsyaddhs.cs.upi.edu.go_pkler";
+    public static final String API_KEY = "AIzaSyC73TEggymYHQ1imRJ2yEJ7oJW4-mSLbtg";
+
+    public static final String SELECT_PEMBELI = "http://giftstoreid.xyz/allpembeli.php";
+    public static final String UPDATE_PEDAGANG = "http://giftstoreid.xyz/updatepedagang.php";
+}
